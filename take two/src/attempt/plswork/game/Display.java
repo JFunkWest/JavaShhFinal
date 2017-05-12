ad38971c6ccc6427fdcb02d4e772851e3f55e632
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-05-12 13:19:31 -0500

    update

--- take two/src/attempt/plswork/game/Display.java
+++ take two/src/attempt/plswork/game/Display.java
@@ -1,15 +1,86 @@
 package attempt.plswork.game;
 
 import java.awt.*;
+
+import java.awt.image.*;
+
 import javax.swing.*;
 
-public class Display extends Canvas {
+import attempt.plswork.game.graphics.Render;
+import attempt.plswork.game.graphics.Screen;
+
+public class Display extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final int WIDTH = 800;
 	public static final int HEIGTH = 600;
 	public static final String TITLE = "PLS WORK GAME";
+
+	private Thread thread;
+	private Screen screen;
+	private BufferedImage img;
+	private boolean running = false;
+	private Render render;
+	private int[] pixels;
 	
+	public Display() {
+		screen = new Screen(WIDTH, HEIGHT);
+		img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
+	}
+
+	private void start() {
+		if (running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	
+	}
+
+	private void stop() {
+		if(!running)
+			return;
+		running = false;
+		try {
+		thread.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void run() {
+		while (running) {
+			tick();
+			render();
+		}
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		screen.render();
+		
+		for (int i = 0; i<WIDTH * HEIGHT; i++) {
+			pixels[1] = screen.pixels[i];
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(img, 0, 0, WIDTH*20, HEIGHT*20, null);
+		g.dispose();
+		bs.show();
+		
+	}
+
 	public static void main(String[] args) {
 		Display game = new Display();
 		JFrame frame = new JFrame();
@@ -17,9 +88,11 @@ public class Display extends Canvas {
 		frame.pack();
 		frame.setTitle(TITLE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
 		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setVisible(true);
+	
+		game.start();
 	}
 }
