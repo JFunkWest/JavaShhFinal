48e052e93fe352244643174dc0739a9bedfec152
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-05-19 13:21:10 -0500

    update

--- Java Final Project/src/finalJava/FlipACoin.java
+++ Java Final Project/src/finalJava/FlipACoin.java
@@ -1,47 +1,61 @@
 package finalJava;
-import java.util.*;
+import javax.swing.JOptionPane;
+import java.util.Random;
 import javax.swing.*;
 public class FlipACoin {
-	 public static void main(String[] args)
-	    {
-	        String input1,
-	        input2;
-	        int choice1,
-	        choice2;
-	        
-	        input1 =
-	        		JOptionPane.showInputDialog("How much money are you willing to bet?");
-	        choice1 = Integer.parseInt(input1);
-	        
-	        input2 =
-	            JOptionPane.showInputDialog("Do you think the coin will be\n 1 for heads\n or\n 2 for tails?");
-	        choice2 = Integer.parseInt(input2);
-	        
-	        coinToss(choice1, choice2);
-	        
-	        JOptionPane.showMessageDialog(null, "Thank you and come again!");
-	        
-	    }
-	    
-	    public static void coinToss(int number)
-	    {
-	        int coinFace,
-	            coinChoice,
-	            result,
-	            wagerResults;
-	    
-	        Random tossMachine = new Random(1);
-	        
-	        if (coinChoice == 1) {
-	        	
-	        }
-	        
-	        else if (coinChoice == 2) {
-	        	
-	        }
-	        
-	        JOptionPane.showMessageDialog(null, "The coin landed on " + 
-	                                      coinFace + ", your guess was " + coinChoice + ". You " + result + "$" + wagerResults + ". Thank you for playing.");
-	        
-	    }
+
+	
+
+	private static final String YES_NO_OPTION = null;
+
+	public static void displayOutcome()
+	{
+	do{
+		Random r = new Random();
+		int computer = r.nextInt(2) + 1;
+		String userPick, computerPick, result = null;
+		String userAnswer, computerAnswer = null;
+		int answerInt,  winnings = 0, lose;
+		
+		
+		userAnswer = JOptionPane.showInputDialog(null, "Choose \n1 for Heads\n2 for Tails");
+		answerInt = Integer.parseInt(userAnswer);
+		
+		if(answerInt == 1)
+		{
+			userAnswer = "Heads";
+			if(computer == 1)
+			{
+				computerAnswer = "Heads";
+				result = "You Win Hahahahaha!";
+				winnings = winnings + 25;
+			}
+			else if(computer  == 2)
+			{
+				computerAnswer = "Tails";
+				result = "You lose, oh no!";
+				winnings = winnings - 25;
+			}
+		}
+		if(answerInt == 2)
+		{
+			userAnswer = "Tails";
+			if(computer == 2)
+			{
+				computerAnswer = "Tails";
+				result = "You Win Hahahahaha!";
+				winnings = winnings + 25;
+			}
+			else if(computer  == 1)
+			{
+				computerAnswer = "Heads";
+				result = "You lose, oh no!";
+				winnings = winnings - 25;
+			}
+		}
+
+		JOptionPane.showMessageDialog(null,  "You picked " + userAnswer + ". The coin landed on " + computerAnswer + ". " + result);
+		JOptionPane.showInputDialog(null,  "Do you want to play again?", YES_NO_OPTION);
+	}while("Yes".equals(YES_NO_OPTION));
 	}
+}
