fe025246ee80c1800754dbb7584b5dd7a89a1470
Nicholas02 <nickantle5@gmail.com>
2017-05-19 12:34:43 -0500

    update

--- Java Final Project/src/finalJava/TicTacToe.java
+++ Java Final Project/src/finalJava/TicTacToe.java
@@ -1,122 +1,114 @@
 package finalJava;
-
-import javax.swing.JOptionPane;
-
-import java.util.Scanner;
+import java.awt.font.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class TicTacToe {
 
-    public static void main(String[] args) {
-
-        char[][] board = {{'1','2','3'}, {'4','5','6'}, {'7', '8', '9'}};
-        // assign player to char value of X's only
-
-        int play;
-
-        char player = 'X';
-
-        char cpu = 'O';
-
-        int rowNumber;
-
-        int columnNumber;
-
-        int playerORow;
-        int playerOcolumn;
-
-        JOptionPane.showMessageDialog(null, "Welcome to tic, tac, toe!\n");
-
-        JOptionPane.showInputDialog(null, "Do you wish to play? 1 for yes, 2 for no ");
-
-        Scanner input = new Scanner(System.in);
-
-        play = input.nextInt();
-
-        if(play != 1) {
-        	JOptionPane.showMessageDialog(null, "Invalid input, game will now EXIT thanks for playing!");
-            System.exit(0);
-        } // end if
-
-            displayBoard(board);
-            JOptionPane.showMessageDialog(null, "You are limited to X's only, good luck!");
-            JOptionPane.showMessageDialog(null, "Please enter row (0-3) of your move: ");
-            rowNumber = input.nextInt();
-            JOptionPane.showMessageDialog(null, "Please enter column (1-3); of your move: ");
-            columnNumber = input.nextInt();
-
-            JOptionPane.showMessageDialog(null, "Please enter row (0-3) for player O: ");
-            playerORow = input.nextInt();
-            JOptionPane.showMessageDialog(null, "Please enter column (1-3); of your move: ");
-            playerOcolumn = input.nextInt();
+	private static int[][] winCombinations = new int[][] {
+			{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //horizontal wins
+			{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //vertical wins
+			{0, 4, 8}, {2, 4, 6}			 //diagonal wins
+	};
+	
+	private static JButton buttons[] = new JButton[9]; //create 9 buttons
 
 
-            if(board[rowNumber][columnNumber] != 'X' && board[rowNumber][columnNumber] != 'O')  {
-                board[rowNumber][columnNumber] = player;
-            } // end if
 
-            else {
-
-            }
-
-
-            makeAMove(board, player);
-            hasWon(board, player);
-            boardFull(board);
-
-} // end main method
-
-// displays only the tic tac toe board
-public static void displayBoard(char[][] board) {
-    // loop for each row
-	JOptionPane.showMessageDialog(null, board[0][0] + " | " + board[0][1] + " | " + board[0][2] + "\n---------");
-	JOptionPane.showMessageDialog(null, board[1][0] + " | " + board[1][1] + " | " + board[1][2] + "\n---------");
-	JOptionPane.showMessageDialog(null, board[2][0] + " | " + board[2][1] + " | " + board[2][2] + "\n");
-
-} // end display board method
-
-// takes board array of values and updates it with valid row and column selected by player..does not return anything
-public static void makeAMove(char[][] board, char player) {
-    displayBoard(board);
+	public static void main (String[] args)
+	{
+		gamePanel(); //launch game
+	}
+	
+	private static void gamePanel(){
+		JFrame frame = new JFrame ("Tic Tac Toe");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
 
-} // end makeAMove method
+		JPanel panel = new JPanel(); //creating a panel with a box like a tic tac toe board
+		panel.setLayout (new GridLayout (3, 3));
+		panel.setBorder (BorderFactory.createLineBorder (Color.gray, 3));
+		panel.setBackground (Color.white);
+
+		for(int i=0; i<=8; i++){ //placing the button onto the board
+			buttons[i] = new MyButton();
+			panel.add(buttons[i]);			
+		}
+
+		frame.getContentPane().add (panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(500, 500);// set frame size and let teh game begin
+	}
+
+	public static int xOrO=0; // used for counting
+
+	private static class MyButton extends JButton 
+	implements ActionListener {//creating own button class because JButton sucks:)
+		
+		int again=1000;//set again at 1000 so we don't make the mistake we can play again
+		boolean win=false; // there is not a win
+		String letter; // x or o
+		public MyButton() {	// creating blank board
+			super();
+			letter=" ";
+			setFont(new Font("Dialog", 1, 60));
+			setText(letter);
+			addActionListener(this);
+		}
+		public void actionPerformed(ActionEvent e) { // placing x or o's
+			if((xOrO%2)==0 && getText().equals(" ") && win==false){
+				letter="X";
+				xOrO=xOrO+1;
+				System.out.println(letter + "\n"+xOrO);
+			} else if((xOrO%2)==1 && getText().equals(" ") && win==false) {
+				letter="O";
+				xOrO=xOrO+1;
+				System.out.println(letter + "\n"+xOrO);
+			} // if user does click on a button that is already played, nothing will happen
+			
+			setText(letter); // place the x or the o on the actual board
+
+			
+			for(int i=0; i<=7; i++){ // check for the winning combinations
+				if( buttons[winCombinations[i][0]].getText().equals(buttons[winCombinations[i][1]].getText()) && 
+					buttons[winCombinations[i][1]].getText().equals(buttons[winCombinations[i][2]].getText()) && 
+					buttons[winCombinations[i][0]].getText() != " "){//the winning is true
+					win = true;
+				}
+			}
+			
+			if(win == true){ // if the game ends let the user know who wins and give option to play again
+				again=JOptionPane.showConfirmDialog(null, letter + " wins the game!  Do you want to play again?",letter + "won!",JOptionPane.YES_NO_OPTION);
+				
+			} else if(xOrO == 9 && win == false){//tie game, announce and ask if the user want to play again
+				again=JOptionPane.showConfirmDialog(null, "The game was tie!  Do you want to play again?","Tie game!",JOptionPane.YES_NO_OPTION);
+				win=true;
+			}	
+			
+			if(again==JOptionPane.YES_OPTION && win==true){ // if the user want to play again clear all the button and start over
+					clearButtons();			
+					win=false;
+			}
+			else if(again==JOptionPane.NO_OPTION){
+				System.exit(0); // exit game if the user do not want to play again
+			}
 
 
-// compare each element in board to see if the char value of 'X' exists
-    // if exists then then return true, else return false
-public static boolean hasWon(char[][] board, char player) {
 
-        // Check if the player has won by checking winning conditions.
-        if (board[0][0] == player && board[0][1] == player && board[0][2] == player || // 1st row
-            board[1][0] == player && board[1][1] == player && board[1][2] == player || // 2nd row
-            board[2][0] == player && board[2][1] == player && board[2][2] == player || // 3rd row
-            board[0][0] == player && board[1][0] == player && board[2][0] == player || // 1st col.
-            board[0][1] == player && board[1][1] == player && board[2][1] == player || // 2nd col.
-            board[0][2] == player && board[1][2] == player && board[2][2] == player || // 3rd col.
-            board[0][0] == player && board[1][1] == player && board[2][2] == player || // Diagonal          \ 
-            board[2][0] == player && board[1][1] == player && board[0][2] == player) //   Diagonal      /
 
-            return true;
+		}
 
-        else {
+	}
+	
+	public static void clearButtons(){
+		
+		for(int i=0; i<=8; i++){// clear all 8 buttons
+			buttons[i].setText(" ");						
+		}
+		xOrO=0; // reset the count
+		
+	}
 
-            return false;
-        }
-
-} // end hasWon method
-
-public static boolean boardFull(char [][] board) {
-
-    if (board[0][0] != '1' && board[0][1] != '2' && board[0][2] != '3' &&
-        board[1][0] != '4' && board[1][1] != '5' && board[1][2] != '6' &&
-        board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9')
-
-        return true;
-
-    else {
-
-        return false;
-    } // end else
-
-	} // end boardFull method
 }
