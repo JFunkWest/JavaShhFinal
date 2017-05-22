df6660f5a09a33a1a064bf8118a50bc49aeeee38
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-05-22 13:20:14 -0500

    update

--- Java Final Project/src/finalJava/FlipACoin.java
+++ Java Final Project/src/finalJava/FlipACoin.java
@@ -1,26 +1,25 @@
 package finalJava;
 import javax.swing.JOptionPane;
+
 import java.util.Random;
+
 import javax.swing.*;
 public class FlipACoin {
 
-	
-
-	private static final String YES_NO_OPTION = null;
-
 	public static void displayOutcome()
 	{
-	do{
+	
+
 		Random r = new Random();
 		int computer = r.nextInt(2) + 1;
 		String userPick, computerPick, result = null;
 		String userAnswer, computerAnswer = null;
-		int answerInt,  winnings = 0, lose;
-		
-		
+		int answerInt,  winnings = 0, lose, moneyBet = 0;
+		String money = JOptionPane.showInputDialog(null, "How much money are you willing to bet?");
+		moneyBet = Integer.parseInt(money);
 		userAnswer = JOptionPane.showInputDialog(null, "Choose \n1 for Heads\n2 for Tails");
 		answerInt = Integer.parseInt(userAnswer);
-		
+
 		if(answerInt == 1)
 		{
 			userAnswer = "Heads";
@@ -28,13 +27,13 @@ public class FlipACoin {
 			{
 				computerAnswer = "Heads";
 				result = "You Win Hahahahaha!";
-				winnings = winnings + 25;
+				winnings = moneyBet + 25;
 			}
 			else if(computer  == 2)
 			{
 				computerAnswer = "Tails";
 				result = "You lose, oh no!";
-				winnings = winnings - 25;
+				winnings = moneyBet - 25;
 			}
 		}
 		if(answerInt == 2)
@@ -44,18 +43,18 @@ public class FlipACoin {
 			{
 				computerAnswer = "Tails";
 				result = "You Win Hahahahaha!";
-				winnings = winnings + 25;
+				winnings = moneyBet + 25;
 			}
 			else if(computer  == 1)
 			{
 				computerAnswer = "Heads";
 				result = "You lose, oh no!";
-				winnings = winnings - 25;
+				winnings = moneyBet - 25;
 			}
 		}
 
-		JOptionPane.showMessageDialog(null,  "You picked " + userAnswer + ". The coin landed on " + computerAnswer + ". " + result);
-		JOptionPane.showInputDialog(null,  "Do you want to play again?", YES_NO_OPTION);
-	}while("Yes".equals(YES_NO_OPTION));
+		JOptionPane.showMessageDialog(null,  "You picked " + userAnswer + ". The coin landed on " + computerAnswer + ". " + result + " Your winnings or loss is $" + winnings + ".");
+	
+	
 	}
 }
