faf8a75a72813d4a2738d007d2740c967b085017
Nicholas02 <nickantle5@gmail.com>
2017-05-23 11:40:37 -0500

    update

---
+++ Java Final Project/src/finalJava/RussianRoulette.java
@@ -0,0 +1,33 @@
+package finalJava;
+
+import javax.swing.JOptionPane;
+
+public class RussianRoulette {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		final int MIN = 1;
+		final int MAX = 6;
+		int random;
+		int guess;
+		boolean isMatch;
+		String guessString;
+		
+		random = MIN + (int)(Math.random() * MAX);
+		
+		guessString = JOptionPane.showInputDialog(null, "Wnter \n1 to pull The Trigger \n2 to cancel", "random guess",JOptionPane.INFORMATION_MESSAGE);
+		
+		guess = Integer.parseInt(guessString);
+		isMatch = guess == random;
+		
+		JOptionPane.showMessageDialog(null, "Your guess was " + (random-guess) + " number away from the random number.");
+		JOptionPane.showMessageDialog(null, "The number was " + random + ". The result is " + isMatch + ".");
+		
+		
+		
+		
+	
+	}
+
+}
